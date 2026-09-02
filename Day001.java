05780a30b91f1dd34063207fb81c296f82579e53
MUH SHAQR DZAKY <neffexx253@gmail.com>
2026-09-02 22:27:47 +0800

    Create Day001.java

---
+++ Day001.java
@@ -0,0 +1,7 @@
+public class Hello {
+  //Hari pertama saya ngoding menggunakan bahasa pemrograman java 
+  //(day 001 ngoding)
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}

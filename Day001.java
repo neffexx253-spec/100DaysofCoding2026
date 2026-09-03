4afd9b362777be7af01ed1ba7d788e63c7992ed6
MUH SHAQR DZAKY <neffexx253@gmail.com>
2026-09-03 12:39:02 +0800

    Update Day001.java

--- Day001.java
+++ Day001.java
@@ -1,6 +1,10 @@
 public class Hello {
   //Hari pertama saya ngoding menggunakan bahasa pemrograman java 
   //(day 001 ngoding)
+  // Ini adalah komentar single-line (satu baris)
+  /*
+     * Ini adalah komentar multi-line (beberapa baris)
+  */
     public static void main(String[] args) {
         System.out.println("Hello World");
     }

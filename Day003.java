f2aef83ebb6514ce7f6c37f556d3d05678833a0e
MUH SHAQR DZAKY <neffexx253@gmail.com>
2026-09-04 20:33:07 +0800

    Create Day003.java

---
+++ Day003.java
@@ -0,0 +1,27 @@
+public class day003 {
+    public static void main(String[] args) {
+        // 1. \n = untuk baris baru
+        System.out.println("Nama: Dzaky\nUmur: 20");
+
+        // 2. \t = tab 1 kali
+        System.out.println("Memberikan \tjarak");
+
+        // 3. \\ = backslash atau memberikan garis miring terbalik
+        System.out.println("Github\\Repository\\100DayofCoding\\Day003.java");
+
+        // 4. \" = tanda petik ganda
+        System.out.println("Hi \"Halo\"");
+
+        // 5. \' = petik tunggal
+        System.out.println("Hari \'Jumat\'");
+
+        // 6. \b = hapus 1 karakter sebelumnya
+        System.out.println("Hello\bWorld"); // Jadi "HalDunia"
+
+        // 7. \r = kembali ke awal baris
+        System.out.println("Java\rCode"); // Jadi "Code"
+
+        // Gabungan semua
+        System.out.println("\n=== DATA ===\nNama\t: \"Dzaky\"\nKota\t: Majene");
+    }
+}

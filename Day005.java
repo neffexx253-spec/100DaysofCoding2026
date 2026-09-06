8923080bd81cc67143dca1ee2152594b7d8ca229
MUH SHAQR DZAKY <neffexx253@gmail.com>
2026-09-06 20:59:28 +0800

    Create Day005.java

---
+++ Day005.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // Float (pakai f di akhir)
+        float tinggibadan = 163.0f;
+
+        // Double (tanpa akhiran)
+        double beratbadan = 61.0;
+
+        // Operasi sederhana
+        tinggibadan = tinggibadan + 2.5f;
+        beratbadan = beratbadan + 2.5;
+
+        // Cetak hasil
+        System.out.println("Float " + "Tinggi Badan:" + tinggibadan + " " + "cm");
+        System.out.println("Double " + " Berat Badan:" + beratbadan + " " + "kg");
+    }
+}
